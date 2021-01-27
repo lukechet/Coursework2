bbdcd38a5176863861f53cdbf12a2230e11beebf
lukechet <73609572+lukechet@users.noreply.github.com>
2021-01-27 14:32:05 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const posts = require('./posts');
-app.use('./posts', posts);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('server started on port 3000'));
